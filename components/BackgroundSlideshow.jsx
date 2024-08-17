1abf3181517e118fdcc4c948d6abd7891ea314c5
Jesse Smith <cappellacoding@gmail.com>
2024-08-17 18:07:31 +0200

    changed base routing

--- components/BackgroundSlideshow.jsx
+++ components/BackgroundSlideshow.jsx
@@ -4,27 +4,27 @@ import "../styles/BackgroundSlideshow.scss"
 export default function BackgroundSlideshow() {
     useEffect(() => {
         const images = [
-            "../comicCollector-3000/images/bg_images/erik-mclean-0_wXoeCDry0-unsplash.jpg",
-            "../comicCollector-3000/images/bg_images/pexels-chuck-3744195.jpg",
-            "../comicCollector-3000/images/bg_images/pexels-chuck-3744196.jpg",
-            "../comicCollector-3000/images/bg_images/pexels-introspectivedsgn-6654169.jpg",
-            "../comicCollector-3000/images/bg_images/pexels-introspectivedsgn-6654171.jpg",
-            "../comicCollector-3000/images/bg_images/pexels-introspectivedsgn-6654172.jpg",
-            "../comicCollector-3000/images/bg_images/pexels-introspectivedsgn-6654173.jpg",
-            "../comicCollector-3000/images/bg_images/pexels-introspectivedsgn-6654177.jpg",
-            "../comicCollector-3000/images/bg_images/pexels-introspectivedsgn-7524992.jpg",
-            "../comicCollector-3000/images/bg_images/pexels-introspectivedsgn-7524993.jpg",
-            "../comicCollector-3000/images/bg_images/pexels-introspectivedsgn-7524996.jpg",
-            "../comicCollector-3000/images/bg_images/pexels-introspectivedsgn-7524998.jpg",
-            "../comicCollector-3000/images/bg_images/pexels-introspectivedsgn-7768661.jpg",
-            "../comicCollector-3000/images/bg_images/pexels-introspectivedsgn-7768663.jpg",
-            "../comicCollector-3000/images/bg_images/pexels-maria-firman-150089027-12113866.jpg",
-            "../comicCollector-3000/images/bg_images/pexels-maria-firman-150089027-12113867.jpg",
-            "../comicCollector-3000/images/bg_images/pexels-nerdcinema-20085947.jpg",
-            "../comicCollector-3000/images/bg_images/pexels-sumeyye-ugurlu-2759284-6807234.jpg",
-            "../comicCollector-3000/images/bg_images/pexels-theshuttervision-8811520.jpg",
-            "../comicCollector-3000/images/bg_images/pexels-theshuttervision-8811521.jpg",
-            "../comicCollector-3000/images/bg_images/pexels-theshuttervision-12328057.jpg"
+            "../images/bg_images/erik-mclean-0_wXoeCDry0-unsplash.jpg",
+            "../images/bg_images/pexels-chuck-3744195.jpg",
+            "../images/bg_images/pexels-chuck-3744196.jpg",
+            "../images/bg_images/pexels-introspectivedsgn-6654169.jpg",
+            "../images/bg_images/pexels-introspectivedsgn-6654171.jpg",
+            "../images/bg_images/pexels-introspectivedsgn-6654172.jpg",
+            "../images/bg_images/pexels-introspectivedsgn-6654173.jpg",
+            "../images/bg_images/pexels-introspectivedsgn-6654177.jpg",
+            "../images/bg_images/pexels-introspectivedsgn-7524992.jpg",
+            "../images/bg_images/pexels-introspectivedsgn-7524993.jpg",
+            "../images/bg_images/pexels-introspectivedsgn-7524996.jpg",
+            "../images/bg_images/pexels-introspectivedsgn-7524998.jpg",
+            "../images/bg_images/pexels-introspectivedsgn-7768661.jpg",
+            "../images/bg_images/pexels-introspectivedsgn-7768663.jpg",
+            "../images/bg_images/pexels-maria-firman-150089027-12113866.jpg",
+            "../images/bg_images/pexels-maria-firman-150089027-12113867.jpg",
+            "../images/bg_images/pexels-nerdcinema-20085947.jpg",
+            "../images/bg_images/pexels-sumeyye-ugurlu-2759284-6807234.jpg",
+            "../images/bg_images/pexels-theshuttervision-8811520.jpg",
+            "../images/bg_images/pexels-theshuttervision-8811521.jpg",
+            "../images/bg_images/pexels-theshuttervision-12328057.jpg"
             // Add more images as needed
         ]
 
