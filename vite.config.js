fefd9c7b15694f604a9be7cf01efd4ff6eb6d9a2
Jesse Smith <cappellacoding@gmail.com>
2024-08-17 19:12:24 +0200

    updates for bug fix

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,10 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./",
   plugins: [react()],
+  base: "./",
   build: {
+    outDir: "dist",
     rollupOptions: {
       output: {
         format: "es", // Ensure ES module format
