122de1af8768482c18e6919db71680eb2e0778be
Jesse Smith <cappellacoding@gmail.com>
2024-08-19 16:09:27 +0200

    updated vite config to try and resolve bug on deployed version

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,13 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: { "process.env": {} },
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+    extensions: [".js", ".json", ".jsx", ".mjs", ".ts", ".tsx"],
+  },
   base: "./",
   build: {
     outDir: "dist",
